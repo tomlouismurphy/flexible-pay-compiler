76362702b5f16558908f0a9d7d83812d86502d8a
tom.louis.murphy <tom.louis.murphy@gmail.com>
2018-06-12 16:32:59 -0500

    Form is functional and interacts with state

--- src/EmployeeForm/EmployeeForm.js
+++ src/EmployeeForm/EmployeeForm.js
@@ -13,10 +13,40 @@ export class EmployeeForm extends Component {
       paymentEndDate: ''
     }
   }
+  handleInput = (e) => {
+    const state = this.state;
+    console.log(e.currentTarget.value);
+    state[e.currentTarget.name] = e.currentTarget.value;
+    this.setState(state);
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
   render() {
     return (
-      <div>
-        <p>Info goes here.</p>
+      <div className="formContainer">
+        <form className="payslipInformation">
+          <label className="payslipTidbit">First Name:</label>
+          <input className="inputBox" type="text" name="firstName" onChange={this.handleInput}/>
+          <br/>
+          <label className="payslipTidbit">Last Name:</label>
+          <input className="inputBox" type="text" name="lastName" onChange={this.handleInput}/>
+          <br/>
+          <label className="payslipTidbit">Annual Salary:</label>
+          <input className="inputBox" type="text" name="annualSalary" onChange={this.handleInput}/>
+          <br/>
+          <label className="payslipTidbit">Super Rate:</label>
+          <input className="inputBox" type="text" name="superannuationRate" onChange={this.handleInput}/>
+          <br/>
+          <label className="payslipTidbit">Pay Period Start:</label>
+          <input className="inputBox" type="text" name="paymentStartDate" onChange={this.handleInput}/>
+          <br/>
+          <label className="payslipTidbit">Pay Period End:</label>
+          <input className="inputBox" type="text" name="paymentEndDate" onChange={this.handleInput}/>
+          <br/>
+          <button className="submitButton" onClick={this.handleSubmit}>Submit</button>
+        </form>
       </div>
     );
   }
